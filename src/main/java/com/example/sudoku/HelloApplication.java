aa7168a5280ca525135d83e328795cea888d468b
Dana Sofia Gomez Manrique <danagomez2816@gmail.com>
2025-10-13 05:09:41 -0500

    Mejoras en la vista del juego

--- src/main/java/com/example/sudoku/HelloApplication.java
+++ src/main/java/com/example/sudoku/HelloApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -15,7 +16,11 @@ public class HelloApplication extends Application {
         Parent root = fxmlLoader.load();
         Scene scene = new Scene(root);
         stage.setTitle("Sudoku");
+        stage.getIcons().add(new Image(
+                getClass().getResourceAsStream("/com/example/sudoku/images/pngwing.com (1).png")
+        ));
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.example.sudoku {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
+    requires java.desktop;
 
 
     opens com.example.sudoku to javafx.fxml;

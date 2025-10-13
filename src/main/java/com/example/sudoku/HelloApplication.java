73b4e2c5a2952355cb439d2e84d883be8792bce1
Dana Sofia Gomez Manrique <danagomez2816@gmail.com>
2025-10-13 04:50:01 -0500

    Se mejoró el diseño del juego y se realizaron cambios en los archivos el juego para comenzar a enlazar la vista con la lógica

--- src/main/java/com/example/sudoku/HelloApplication.java
+++ src/main/java/com/example/sudoku/HelloApplication.java
@@ -2,6 +2,7 @@ package com.example.sudoku;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -11,8 +12,9 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("inicio-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+        stage.setTitle("Sudoku");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,10 @@
 module com.example.sudoku {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
 
     opens com.example.sudoku to javafx.fxml;
+    opens com.example.sudoku.controllers to javafx.fxml;
     exports com.example.sudoku;
 }

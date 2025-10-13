d27cdf2bd467d2bf3590c53b283a89b7e417f334
Dana Sofia Gomez Manrique <danagomez2816@gmail.com>
2025-10-13 04:52:44 -0500

    Creación de los controller junto con los componentes de los FXML

--- src/main/java/com/example/sudoku/controllers/InicioController.java
+++ src/main/java/com/example/sudoku/controllers/InicioController.java
@@ -1,4 +1,42 @@
 package com.example.sudoku.controllers;
 
+import com.example.sudoku.models.AlertBox;
+import com.example.sudoku.models.IAlertBox;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class InicioController {
+
+    private IAlertBox alertBox;
+
+    @FXML
+    private Button btnJugar;
+
+    @FXML
+    private Button btnReglas;
+
+    @FXML
+    void onActionJugar(ActionEvent event) {
+/*
+        SudokuView sudokuView = SudokuView.getInstance();
+        Sudoku.show();
+
+        // Close stage
+        Node source = (Node)event.getSource();
+        Stage stage = (Stage)source.getScene().getWindow();
+        stage.close();
+
+        SudokuController sudokuController = sudokuView.getSudokuController(); */
+
+    }
+
+    @FXML
+    void onActionReglas(ActionEvent event) {
+        IAlertBox alertBox = new AlertBox();
+        alertBox.mostrarReglasSudoku();
+    }
+
 }
+
+
--- src/main/java/com/example/sudoku/controllers/SudokuController.java
+++ src/main/java/com/example/sudoku/controllers/SudokuController.java
@@ -1,4 +1,149 @@
 package com.example.sudoku.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+
 public class SudokuController {
+
+    @FXML
+    private Button btnAyuda;
+
+    @FXML
+    private Button btnNuevoJuego;
+
+    @FXML
+    private Button btnVerificar;
+
+    @FXML
+    private GridPane gridpaneSudoku;
+
+    @FXML
+    private GridPane gridpaneb1;
+
+    @FXML
+    private GridPane gridpaneb2;
+
+    @FXML
+    private GridPane gridpaneb3;
+
+    @FXML
+    private GridPane gridpaneb4;
+
+    @FXML
+    private GridPane gridpaneb5;
+
+    @FXML
+    private GridPane gridpaneb6;
+
+    @FXML
+    private TextField textField00;
+
+    @FXML
+    private TextField textField01;
+
+    @FXML
+    private TextField textField02;
+
+    @FXML
+    private TextField textField03;
+
+    @FXML
+    private TextField textField04;
+
+    @FXML
+    private TextField textField05;
+
+    @FXML
+    private TextField textField10;
+
+    @FXML
+    private TextField textField11;
+
+    @FXML
+    private TextField textField12;
+
+    @FXML
+    private TextField textField13;
+
+    @FXML
+    private TextField textField14;
+
+    @FXML
+    private TextField textField15;
+
+    @FXML
+    private TextField textField20;
+
+    @FXML
+    private TextField textField21;
+
+    @FXML
+    private TextField textField22;
+
+    @FXML
+    private TextField textField23;
+
+    @FXML
+    private TextField textField24;
+
+    @FXML
+    private TextField textField25;
+
+    @FXML
+    private TextField textField30;
+
+    @FXML
+    private TextField textField31;
+
+    @FXML
+    private TextField textField32;
+
+    @FXML
+    private TextField textField33;
+
+    @FXML
+    private TextField textField34;
+
+    @FXML
+    private TextField textField35;
+
+    @FXML
+    private TextField textField40;
+
+    @FXML
+    private TextField textField41;
+
+    @FXML
+    private TextField textField42;
+
+    @FXML
+    private TextField textField43;
+
+    @FXML
+    private TextField textField44;
+
+    @FXML
+    private TextField textField45;
+
+    @FXML
+    private TextField textField50;
+
+    @FXML
+    private TextField textField51;
+
+    @FXML
+    private TextField textField52;
+
+    @FXML
+    private TextField textField53;
+
+    @FXML
+    private TextField textField54;
+
+    @FXML
+    private TextField textField55;
+
 }
+

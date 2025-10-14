cef06bea222d3982e96e9204082ddaf221f7549c
Miguel Angel Lasso Troncoso <lassomiguel1021@gmail.com>
2025-10-13 22:15:24 -0500

    Se configuro el InicioView

--- src/main/java/com/example/sudoku/views/InicioView.java
+++ src/main/java/com/example/sudoku/views/InicioView.java
@@ -1,4 +1,32 @@
 package com.example.sudoku.views;
 
-public class InicioView {
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class InicioView extends Stage {
+
+    public InicioView() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(
+            getClass().getResource("/com/example/sudoku/inicio-view.fxml")
+        );
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+        this.setScene(scene);
+        this.setTitle("Sudoku");
+    }
+
+    public static InicioView getInstance() throws IOException {
+        if (InicioView.InicioViewHolder.INSTANCE == null) {
+            InicioView.InicioViewHolder.INSTANCE = new InicioView();
+        }
+        return InicioView.InicioViewHolder.INSTANCE;
+    }
+
+    private static class InicioViewHolder {
+        private static InicioView INSTANCE = null;
+    }
 }

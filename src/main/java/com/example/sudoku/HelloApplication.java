4e3a0d4ed93eefa3ca8e0973798ba15beb45da6b
Miguel Angel Lasso Troncoso <lassomiguel1021@gmail.com>
2025-10-16 23:35:23 -0500

    Se configuro para llamar al InicioView desde aqui

--- src/main/java/com/example/sudoku/HelloApplication.java
+++ src/main/java/com/example/sudoku/HelloApplication.java
@@ -1,8 +1,7 @@
 package com.example.sudoku;
 
+import com.example.sudoku.views.InicioView;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,10 +9,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("inicio-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+
+        InicioView inicioView = InicioView.getInstance();
+        inicioView.show();
+
     }
 }
